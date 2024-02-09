c5b5d60c53686f064a3c9e2871f4f48fc6ca516d
Zephyritz <srivastava.ritwik@yahoo.in>
2024-02-09 11:44:54 +0530

    Update ConsoleLogToWorkspace.java
    Updated with logic to close off console writing when a certain time / size is reached

--- src/main/java/hudson/plugins/ConsoleLogToWorkspace/ConsoleLogToWorkspace.java
+++ src/main/java/hudson/plugins/ConsoleLogToWorkspace/ConsoleLogToWorkspace.java
@@ -10,9 +10,11 @@ import java.io.OutputStream;
 import hudson.EnvVars;
 
 public class ConsoleLogToWorkspace {
+    private static final int DEFAULT_LOG_SIZE_LIMIT = 1024 * 1024; // 1 MB
+    private static final int DEFAULT_TIMEOUT_SECONDS = 300; // 5 minutes
 
     public static boolean perform(Run<?, ?> build, FilePath workspace, TaskListener listener,
-            boolean writeConsoleLog, String fileName, boolean blockOnAllOutput) {
+            boolean writeConsoleLog, String fileName, boolean blockOnAllOutput, int logSizeLimit, int timeoutSeconds) {
         final OutputStream os;
 
         try {
@@ -37,13 +39,35 @@ public class ConsoleLogToWorkspace {
         listener.getLogger().println("[ConsoleLogToWorkspace] " + message);
     }
 
+    // Added new log function to accommodate for timeout and line check blocks
+    private static void log(String message, OutputStream out) throws IOException {
+        out.write(("[ConsoleLogToWorkspace] " + message + "\n").getBytes());
+    }
     private static void writeLogFile(AnnotatedLargeText logText, OutputStream out,
-            boolean block) throws IOException, InterruptedException {
+                                     boolean block, int logSizeLimit, int timeoutSeconds) throws IOException, InterruptedException {
         long pos = 0;
         long prevPos = pos;
+        long startTime = System.currentTimeMillis();
+
         do {
             prevPos = pos;
             pos = logText.writeLogTo(pos, out);
+            long elapsedTime = System.currentTimeMillis() - startTime;
+
+            // Check log size limit
+            if (pos > logSizeLimit) {
+                log("Reached log size limit of " + logSizeLimit + " bytes. Aborting.",out);
+                out.write("\nACID: log truncated (size exceeded)\n".getBytes());
+                break;
+            }
+
+            // Check timeout
+            if (block && elapsedTime > timeoutSeconds) {
+                log("Timeout exceeded (" + timeoutSeconds + " seconds). Aborting.",out);
+                out.write("\nACID: log truncated (timeout)\n".getBytes());
+                break;
+            }
+
             // Nothing new has been written or not blocking
             if (prevPos >= pos || !block) {
                 break;
